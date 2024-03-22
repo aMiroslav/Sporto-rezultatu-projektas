cab02e6dde76391975b4422422b23f3a2a6edbe4
DaivaGo <dgostautaite@gmail.com>
2024-03-22 11:32:09 +0200

    krepsinis.py

--- krepsinis.py
+++ krepsinis.py
@@ -8,6 +8,16 @@ import seaborn as sns
 
 df = pd.read_csv('eurolyga.csv')
 
+
+daugiausiai_tasku_imetusiuos_top_10_komandos = df.groupby('Komanda')['Vidut_tasku_per_rungtyne'].max().sort_values(ascending=False).head(10)
+# print(daugiausiai_tasku_imetusiuos_top_10_komandos)
+
+komandos_geriausiai_zaidusios_namie = df.groupby('Komanda')['Laimeta_namie'].sum().sort_values(ascending=False)
+# print(komandos_geriausiai_zaidusios_namie)
+
+dvieju_geriausiu_komandu_palyginimas = df.loc[0].compare(df.loc[1])
+# print(dvieju_geriausiu_komandu_palyginimas)
+
 top_5_komandos = df.groupby('Komanda')['Laimeta'].mean().sort_values(ascending=False).head(5)
 # print(top_5_komandos)
 
@@ -30,5 +40,6 @@ plt.xlabel('Komanda', fontsize=14)
 plt.ylabel('Pralaimetos rungtynes', fontsize=14)
 plt.xticks(rotation=1)
 plt.grid(True)
-plt.show()
+# plt.show()
+
 
